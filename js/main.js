3cc3663f3fe30c7e21e43fcaaed20e503ba8757e
Adi-Davidovich <adidaflower@gmail.com>
2021-10-18 21:29:26 +0300

    added model and render logic

--- js/main.js
+++ js/main.js
@@ -1,1 +1,35 @@
 console.log('Starting up');
+
+$(onInit());
+
+function onInit() {
+    renderProjs()
+}
+
+function renderProjs() {
+    var projs = gProjs;
+    var strHtml = projs.map(proj => {
+        return `<div class="col-md-4 col-sm-6 portfolio-item">
+        <a class="portfolio-link" data-toggle="modal" href="#portfolioModal${capitalize(proj.id)}">
+                    <div class="portfolio-hover">
+                        <div class="portfolio-hover-content">
+                            <i class="fa fa-plus fa-3x"></i>
+                        </div>
+                    </div>
+                    <img class="img-fluid" src="img/portfolio/${proj.id}-thumbnail.jpg" alt="">
+                </a>
+                <div class="portfolio-caption">
+                    <h4>${proj.name}</h4>
+                    <p class="text-muted">${proj.title}</p>
+                </div></div>`
+    });
+
+    $('.portfolio-container').html(strHtml);
+    console.log(strHtml);
+}
+
+
+
+function capitalize(s) {
+    return s[0].toUpperCase() + s.slice(1);
+}
--- js/proj-service.js
+++ js/proj-service.js
@@ -1,3 +1,63 @@
 'use strict'
 
-var gProjs
+var gProjs = createProjs();
+
+function createProjs() {
+    var projs = [
+        {
+            id: "ball-board",
+            name: "Ball Board",
+            title: "Collect those balls",
+            desc: "lorem ipsum lorem ipsum lorem ipsum",
+            url: "projs/ball-board",
+            publishedAt: 1448693940000,
+            labels: ["Matrixes", "keyboard events"]
+        },
+        {
+            id: "balloon-pop",
+            name: "Balloon Pop",
+            title: "Can you pop all the balloons?",
+            desc: "lorem ipsum lorem ipsum lorem ipsum",
+            url: "projs/balloon-pop",
+            publishedAt: 1448693940000,
+            labels: ["Matrixes", "keyboard events"]
+        },
+        {
+            id: "guess-me",
+            name: "Guess Me",
+            title: "The guessing game",
+            desc: "lorem ipsum lorem ipsum lorem ipsum",
+            url: "projs/guessme",
+            publishedAt: 1448693940000,
+            labels: ["Matrixes", "keyboard events"]
+        },
+        {
+            id: "mineswipper",
+            name: "Mineswipper",
+            title: "The mineswipper game",
+            desc: "lorem ipsum lorem ipsum lorem ipsum",
+            url: "projs/mineswipper",
+            publishedAt: 1448693940000,
+            labels: ["Matrixes", "keyboard events"]
+        },
+        {
+            id: "pacman",
+            name: "Pacman",
+            title: "The pacman game",
+            desc: "lorem ipsum lorem ipsum lorem ipsum",
+            url: "projs/pacman",
+            publishedAt: 1448693940000,
+            labels: ["Matrixes", "keyboard events"]
+        },
+        {
+            id: "touch-nums",
+            name: "Touch Numbers",
+            title: "Choose the correct number",
+            desc: "lorem ipsum lorem ipsum lorem ipsum",
+            url: "projs/touch-nums",
+            publishedAt: 1448693940000,
+            labels: ["Matrixes", "keyboard events"]
+        }
+    ]
+    return projs;
+}
