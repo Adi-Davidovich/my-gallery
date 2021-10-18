47e808ca09648c55a9bcd4e558490aa2366afc88
Adi-Davidovich <adidaflower@gmail.com>
2021-10-18 21:43:25 +0300

    added func render modal

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ $(onInit());
 
 function onInit() {
     renderProjs()
+    renderModals()
 }
 
 function renderProjs() {
@@ -25,9 +26,50 @@ function renderProjs() {
     });
 
     $('.portfolio-container').html(strHtml);
-    console.log(strHtml);
 }
 
+function renderModals() {
+    var projs = gProjs;
+    var strHtml = projs.map(proj => {
+        return `<div class="portfolio-modal modal fade" id="portfolioModal${capitalize(proj.id)}" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="close-modal" data-dismiss="modal">
+              <div class="lr">
+                <div class="rl"></div>
+              </div>
+            </div>
+            <div class="container">
+              <div class="row">
+                <div class="col-lg-8 mx-auto">
+                  <div class="modal-body">
+                    <!-- Project Details Go Here -->
+                    <h2>${proj.name}</h2>
+                    <p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
+                    <img class="img-fluid d-block mx-auto" src="img/portfolio/${proj.id}-full.jpg" alt="">
+                    <p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est
+                      blanditiis
+                      dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae
+                      cupiditate,
+                      maiores repudiandae, nostrum, reiciendis facere nemo!</p>
+                    <ul class="list-inline">
+                      <li>Date: ${proj.publishedAt}</li>
+                      <li>Client: Threads</li>
+                      <li>Category: Illustration</li>
+                    </ul>
+                    <button class="btn btn-primary" data-dismiss="modal" type="button">
+                      <i class="fa fa-times"></i>
+                      Close Project</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>`
+    })
+    $('.modal-container').html(strHtml);
+}
 
 
 function capitalize(s) {
